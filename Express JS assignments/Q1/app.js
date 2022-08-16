c330e11aeb7804fcee4362f1b27b9c92d4477f5c
duttasaikiran <98551652+duttasaikiran@users.noreply.github.com>
2022-08-16 13:58:33 +0530

    Add files via upload

---
+++ Express JS assignments/Q1/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3000, ()=>{
+  console.log("now listening to port 3000");
+
+})
+
+app.get('/', (req, res)=>{
+  res.send("Hello, world!");
+})
